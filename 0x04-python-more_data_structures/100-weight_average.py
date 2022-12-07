74f3b5351b58228870c97c5ecada18cdc2db2532
Tochukwu Onah <onahpatience1610@gmail.com>
2022-12-07 14:52:55 -0800

    upated

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,17 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
+
 def weight_average(my_list=[]):
-    if my_list and len(my_list):
-        num = 0
-        denom = 0
-        for tup in my_list:
-            num += (tup[0] * tup[1])
-            denom += (tup[1])
-        return (num/denom)
-    return 0
+    """
+    Find the average of weighted ints
+    """
+    if my_list is None or my_list == []:
+        return 0
+    s_xy = sum(map(lambda a: a[0] * a[1], my_list))
+    s_x = sum(map(lambda a: a[1], my_list))
+    return s_xy / s_x
+
+
+if __name__ == '__main__':
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    result = weight_average(my_list)
+    print("Average: {:0.2f}".format(result))
