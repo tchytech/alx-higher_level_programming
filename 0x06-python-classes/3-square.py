7f9bdefefd49b605a33f25c8ea4b29753380e2d3
Tochukwu Onah <onahpatience1610@gmail.com>
2022-12-20 18:41:05 -0800

    done

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,30 +1,31 @@
-3-square.py
-Yesterday
-Tue 12:11 PM
-
-Ojo Samuel uploaded an item
-Text
-3-square.py
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"""Square class to represent a square"""
 
 
 class Square:
-    """Represent a square."""
+    """
+    Defines a Square and its basic properties
+    >>> square_1 = Square()
+    >>> square_2 = Square(7)
+    """
 
-    def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
+    def __init__(self, size=0) -> None:
         """
-        if not isinstance(size, int):
+        Innitialize the size of the square. the size can be specified.
+        If they are not, the size defaults to 0
+        :param size: int size of square ( > 0)
+        """
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
 
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+    def area(self) -> int:
+        """
+        Calculates and returns the area of the square
+        :return: the area of the square
+        """
+
+        return self.__size ** 2
