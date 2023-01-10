7933a8c8405d4437f6b2ebb5a4eda534fda1c8cd
Onah Patience Tochukwu <65729360+tchytech@users.noreply.github.com>
2023-01-10 01:12:34 +0100

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,5 +12,4 @@ class Rectangle(BaseGeometry):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
-        self.__height = height:wq
-            
+        self.__height = height
